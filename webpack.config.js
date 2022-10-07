f7ea5eb344c6fadebe2bfa46c455f771082abc84
sahfe <alam.shafee@gmail.com>
2022-10-07 12:04:52 +0600

    initial project

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path");
 // const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     port: 9000,
   },
   // externals: [nodeExternals()],
