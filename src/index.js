fd4a16fd199a2afe25c3f4480fcfd565a5eaa70a
sahfe <alam.shafee@gmail.com>
2022-10-07 12:25:00 +0600

    function composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("this is working");
+import { compose, pipe } from "lodash/fp";
+
+let input = "javascript";
+let output = "<div>" + input.trim() + "</div>";
+const trim = (str) => str.trim(input);
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+//const result = wrapInDiv(toLowerCase(trim(input)));
+//problem with this approach is its left to right and parenthesis
+// lodash have compose and pipe function to solve this problem
+
+//=====using lodash to solve this problem
+
+//const transform = compose(wrapInDiv, toLowerCase, trim); //again its left to right
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+
+console.log(transform(input));
