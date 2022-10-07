0b41e6b5eb0384f6e645ece6377d575151e94ef5
sahfe <alam.shafee@gmail.com>
2022-10-07 22:35:36 +0600

    creating a store

--- src/reducer.js
+++ src/reducer.js
@@ -3,21 +3,23 @@
 let lastId = 0;
 
 // reducer using switch
-switch (action.type) {
-  case "bugAdded":
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  case "bugRemoved":
-    return state.filter((bug) => bug.id !== action.payload.id);
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
 
-  default:
-    return state;
+    default:
+      return state;
+  }
 }
 
 // // reducer with if else
